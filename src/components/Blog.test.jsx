7e5a65cdcb27156c0ffb155e463bc46cef3e9486
Nuul <nuul@example.com>
2026-05-15 16:21:55 -0600

    Add 'URL and likes displayed after button is pressed' test for Blog component

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,26 +1,60 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { test, expect } from 'vitest'
 import Blog from './Blog'
 
-test('display title and author only', () => {
-  const blog = {
-    title: 'A title',
-    author: 'An author',
-    url: 'anurl.com',
-    likes: 0
+const testBlog = {
+  title: 'A title',
+  author: 'An author',
+  url: 'anurl.com',
+  likes: 0,
+  user: {
+    username: 'AUserName',
+    name: 'A name',
   }
+}
+
+test('display title and author only', () => {
+  const blog = { ...testBlog }
 
   render(<Blog blog={blog}/>)
 
-  const titleElement = screen.getByText('A title', { exact:false })
+  const titleElement = screen.queryByText(blog.title, { exact:false })
   expect(titleElement).toBeDefined()
 
-  const authorElement = screen.getByText('An author', { exact:false })
+  const authorElement = screen.queryByText(blog.author, { exact:false })
   expect(authorElement).toBeDefined()
 
-  const urlElement = screen.queryByText('anurl.com', { exact:false })
+  const urlElement = screen.queryByText(blog.url, { exact:false })
   expect(urlElement).not.toBeInTheDocument()
 
   const likeElement = screen.queryByText('Likes', { exact:false })
   expect(likeElement).not.toBeInTheDocument()
+
+  const usernameElement = screen.queryByText(blog.user.username, { exact:false })
+  expect(usernameElement).not.toBeInTheDocument()
+})
+
+test('URL and likes displayed after button is pressed', async () => {
+  const blog = { ...testBlog }
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const titleElement = screen.queryByText(blog.title, { exact:false })
+  expect(titleElement).toBeDefined()
+
+  const authorElement = screen.queryByText(blog.author, { exact:false })
+  expect(authorElement).toBeDefined()
+
+  const urlElement = screen.queryByText(blog.url, { exact:false })
+  expect(urlElement).toBeInTheDocument()
+
+  const likeElement = screen.queryByText('Likes', { exact:false })
+  expect(likeElement).toBeInTheDocument()
+
+  const usernameElement = screen.queryByText(blog.user.username, { exact:false })
+  expect(usernameElement).toBeInTheDocument()
 })
